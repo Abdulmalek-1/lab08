bb049bd87697174f615e96d3bb9548eab2925561
Abdulmalek Aldossery <Abdulmalekaldossery@gmail.com>
2024-11-03 17:45:33 +0300

    Added

--- task3/task3.c
+++ task3/task3.c
@@ -2,15 +2,16 @@
 #include <stdlib.h>
 
 void conversion(char binary[], char hexa[]) {
-    int first_half = (binary[3] - '0') * 8 + (binary[2] - '0') * 4 +
-                     (binary[1] - '0') * 2 + (binary[0] - '0');
+    int second_half = (binary[3] - '0') * 8 + (binary[2] - '0') * 4 +
+                      (binary[1] - '0') * 2 + (binary[0] - '0');
+    int first_half = (binary[7] - '0') * 8 + (binary[6] - '0') * 4 +
+                     (binary[5] - '0') * 2 + (binary[4] - '0');
+    
     if (first_half < 10) 
         hexa[0] = first_half + '0';
     else 
         hexa[0] = first_half - 10 + 'A';
-
-    int second_half = (binary[7] - '0') * 8 + (binary[6] - '0') * 4 +
-                      (binary[5] - '0') * 2 + (binary[4] - '0');
+        
     if (second_half < 10) 
         hexa[1] = second_half + '0';
     else 
@@ -20,13 +21,13 @@ void conversion(char binary[], char hexa[]) {
 }
 
 void Bin2Hexa() {
-    FILE* ptr1 = fopen("/home/it/chip_design/CommonCore/ProblemSolving/lab08/task3/bitstream.bit", "r");
+    FILE* ptr1 = fopen("bitstream.bit", "r");
     if (!ptr1) {
         printf("Error: file did not open!\n");
         exit(1);
     }
     
-    FILE* ptr2 = fopen("/home/it/chip_design/CommonCore/ProblemSolving/lab08/task3/bitstream.hex", "w");
+    FILE* ptr2 = fopen("bitstream.hex", "w");
     if (!ptr2) {
         printf("Error: output file did not open!\n");
         fclose(ptr1);
@@ -40,7 +41,7 @@ void Bin2Hexa() {
     char temp;
 
     while ((temp = fgetc(ptr1)) != EOF) {
-        if(temp != '\n'){
+        if (temp != '\n') {
             binary[i++] = temp;
             count++;
 
@@ -52,13 +53,15 @@ void Bin2Hexa() {
             }
         }
     }
-    if(count != 0){
-        while(i < 8){
+
+    if (count != 0) {
+        while (i < 8) {
             binary[i++] = '0';
         }
         conversion(binary, hexa);
         fprintf(ptr2, "%s\n", hexa);
     }
+
     fclose(ptr1);
     fclose(ptr2);
 }
